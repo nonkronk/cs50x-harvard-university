7c1e9f9e88f300f8670753ad9af44532ab4a5b07
nonkronk <nonkronkblues@gmail.com>
2020-04-25 17:55:11 +0700

    mario.py

---
+++ pset6/mario/less/mario.py
@@ -0,0 +1,18 @@
+# Import regex
+import re
+
+# Condition loop
+while True:
+    height = input("Height: ")
+    if re.match("^([1-8])", height) is not None:
+        height = int(height)
+        break
+        
+# Builds mario blocks
+for i in range(height):
+    for j in range(height - 1, 0, -1):
+        if (j > i):
+            print(" ", end="")
+    for k in range(i + 1): 
+        print("#", end="")
+    print("")
