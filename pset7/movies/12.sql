cf0151e55f6c53abdcc14d9d8bffe4458cd827fb
nonkronk <nonkronkblues@gmail.com>
2020-04-28 01:39:03 +0700

    Fix the algorithm

--- pset7/movies/12.sql
+++ pset7/movies/12.sql
@@ -1,15 +1,14 @@
-SELECT t1.title
-FROM (SELECT title
-  FROM movies
-  WHERE id IN (SELECT movie_id
+--write a SQL query to list the titles of all movies in which both Johnny Depp and Helena Bonham Carter starred
+
+SELECT t2.title
+FROM (SELECT movie_id, rating
+  FROM ratings
+  WHERE movie_id IN (SELECT movie_id
   FROM stars
   WHERE person_id IN (SELECT id
   FROM people
-  WHERE name = "Johnny Depp"))) as t1, (SELECT title
-  FROM movies
-  WHERE id IN (SELECT movie_id
-  FROM stars
-  WHERE person_id IN (SELECT id
-  FROM people
-  WHERE name = "Helena Bonham Carter"))) as t2
-WHERE t1.title = t2.title;
+  WHERE name = "Chadwick Boseman"))) as t1, (SELECT id, title
+  FROM movies) as t2
+WHERE t1.movie_id = t2.id
+ORDER BY rating DESC
+LIMIT 5;
